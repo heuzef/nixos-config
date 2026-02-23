f282f7622f505786bcd2288122a8044dc56e5637
Heuzef <contact@heuzef.com>
2026-02-23 13:24:40 +0100

    Update AppImages

--- appimages_deploy.sh
+++ appimages_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 APPIMAGES_URLS=(
-    "https://github.com/cjpais/Handy/releases/download/v0.6.9/Handy_0.6.9_amd64.AppImage" # https://handy.computer
-    "https://api.hayase.watch/files/linux-hayase-6.4.48-linux.AppImage" # https://hayase.watch/download
+    "https://github.com/cjpais/Handy/releases/download/v0.6.9/Handy_0.7.6_amd64.AppImage" # https://handy.computer
+    "https://api.hayase.watch/files/linux-hayase-6.4.56-linux.AppImage" # https://hayase.watch/download
     "https://github.com/DantSu/Telmi-Sync/releases/download/0.14.0/Telmi.Sync-0.15.1.AppImage" # https://telmi.fr/#download
 )
 
