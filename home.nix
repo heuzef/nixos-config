532d12d6710f056ba8a535cad847df1eafe0b715
Heuzef <contact@heuzef.com>
2026-03-03 22:43:36 +0100

    Adding LocalForward rules for OpenClaw

--- home.nix
+++ home.nix
@@ -97,7 +97,7 @@
         { name = "vault"; hostName = "192.168.0.103"; }
         { name = "budget"; hostName = "192.168.0.104"; }
         { name = "notes"; hostName = "192.168.0.105"; }
-        { name = "claw"; hostName = "192.168.0.106"; }
+        { name = "claw"; hostName = "192.168.0.106"; extraConfig = "LocalForward 18789 127.0.0.1:18789"; }
         { name = "files"; hostName = "192.168.0.110"; }
         { name = "www"; hostName = "192.168.0.122"; }
         { name = "media"; hostName = "192.168.0.190"; }
