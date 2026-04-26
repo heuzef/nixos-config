ecd43c15e805758eb04c4ecd1fd02d1a4c40358a
Heuzef <contact@heuzef.com>
2026-04-26 13:50:27 +0200

    Update AppImage deploy

--- appimages_deploy.sh
+++ appimages_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 APPIMAGES_URLS=(
-    "https://github.com/cjpais/Handy/releases/download/v0.7.9/Handy_0.7.9_amd64.AppImage" # https://handy.computer
-    "https://api.hayase.watch/files/linux-hayase-6.4.56-linux.AppImage" # https://hayase.watch/download
-    "https://github.com/DantSu/Telmi-Sync/releases/download/0.15.1/Telmi.Sync-0.15.1.AppImage" # https://telmi.fr/#download
+    "https://github.com/cjpais/Handy/releases/download/v0.8.2/Handy_0.8.2_amd64.AppImage" # https://handy.computer
+    "https://api.hayase.watch/files/linux-hayase-6.4.60-linux.AppImage" # https://hayase.watch/download
+    "https://github.com/DantSu/Telmi-Sync/releases/download/0.17.0/Telmi.Sync-0.17.0.AppImage" # https://telmi.fr/#download
 )
 
 cd appimages/
@@ -16,3 +16,7 @@ done
 
 sudo chmod +x -R .
 cp -v ./*.desktop ~/.local/share/applications/
+
+# Todo : improve this script with lastversion
+# Eg : lastversion https://github.com/DantSu/telmi-sync ; lastversion https://github.com/cjpais/Handy
+# https://lastversion.getpagespeed.com/
