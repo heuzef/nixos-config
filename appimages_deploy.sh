7fa3dbb415775299761703657b8e8d22d3e33a37
Heuzef <contact@heuzef.com>
2026-05-02 01:34:37 +0200

    Revert "Update appimages_deploy.sh"
    This reverts commit 80bff1fc1047c392fbade844e7d84dc63b00a616.

--- appimages_deploy.sh
+++ appimages_deploy.sh
@@ -1,55 +1,22 @@
 #!/bin/bash
-# Requirements : wget, lastversion
-# Configuration
-APPIMAGE_DIR="appimages"
-mkdir -p "$APPIMAGE_DIR"
-
-# Specific versions variables
-HAYASE_VERSION="6.4.60"
-
-# Applications (Key = Local filename | Value = GitHub repo or direct URL)
-declare -A APPS
-APPS=(
-    ["telmi-Sync"]="DantSu/Telmi-Sync" # https://telmi.fr/#download
-    ["hayase"]="https://api.hayase.watch/files/linux-hayase-${HAYASE_VERSION}-linux.AppImage" # https://hayase.watch/download
+APPIMAGES_URLS=(
+    "https://github.com/cjpais/Handy/releases/download/v0.8.2/Handy_0.8.2_amd64.AppImage" # https://handy.computer
+    "https://api.hayase.watch/files/linux-hayase-6.4.60-linux.AppImage" # https://hayase.watch/download
+    "https://github.com/DantSu/Telmi-Sync/releases/download/0.17.0/Telmi.Sync-0.17.0.AppImage" # https://telmi.fr/#download
 )
 
-echo "Starting Update Process"
+cd appimages/
+mkdir -p ~/.local/share/applications/
+rm -vf ./*.AppImage
 
-# Clean up old AppImage files to prevent conflicts
-rm -f "$APPIMAGE_DIR"/*.AppImage
-
-for APP_NAME in "${!APPS[@]}"; do
-    SOURCE="${APPS[$APP_NAME]}"
-
-    echo "--------------------"
-    echo "Processing: $APP_NAME..."
-
-    # Check if the source is a direct URL or a GitHub repo
-    if [[ $SOURCE == http* ]]; then
-        # Direct URL (using the version variable defined above)
-        URL=$SOURCE
-    else
-        # Use lastversion for GitHub repositories
-        echo "Fetching latest release for $SOURCE..."
-        # We filter for 'AppImage' assets specifically
-        URL=$(lastversion get "$SOURCE" --assets --filter "AppImage")
-    fi
-
-    if [ -n "$URL" ]; then
-        echo "Downloading: $URL"
-        wget -c -P "$APPIMAGE_DIR/" "$URL"
-    else
-        echo "Error: Could not find AppImage URL for $APP_NAME"
-    fi
+for url in "${APPIMAGES_URLS[@]}"; do
+    echo "Download : $url"
+    wget -c -P "./" "$url"
 done
 
-# Set executable permissions
-chmod +x -R "$APPIMAGE_DIR"/*
+sudo chmod +x -R .
+cp -v ./*.desktop ~/.local/share/applications/
 
-# Copy .desktop files
-cp -v "$APPIMAGE_DIR"/*.desktop ~/.local/share/applications/
-
-echo "--------------------"
-echo "Update Completed!"
-ls -larth "$APPIMAGE_DIR"/
+# Todo : improve this script with lastversion
+# Eg : lastversion https://github.com/DantSu/telmi-sync ; lastversion https://github.com/cjpais/Handy
+# https://lastversion.getpagespeed.com/
