d0b2df9f4a63c469ffe783075bea4fe98c948a38
Heuzef <contact@heuzef.com>
2026-06-14 17:54:18 +0200

    Update Install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-ln -s ~/GIT/nixos-config/fonts/* ~/.local/share/fonts/ # Sync fonts
+#!/bin/sh
+# Build
 sh ~/GIT/nixos-config/rebuild.sh fast
+
+# Get AppImages
 sh ~/GIT/nixos-config/appimages_deploy.sh
+
+# Sync fonts
+mkdir ~/.local/share/fonts/
+ln -s ~/GIT/nixos-config/fonts/* ~/.local/share/fonts/
+
 reboot
